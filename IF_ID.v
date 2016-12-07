1b13f6d369dfe3c4a72e3ec533b0dda9e4953ac6
Bookgin <dongsheoil@gmail.com>
2016-12-07 21:52:16 +0800

    Refine IF_ID to output registers

--- IF_ID.v
+++ IF_ID.v
@@ -1,22 +1,14 @@
-  module IF_ID
-  (
-    clk_i,
-    inst_i,
-    pc_i,
-    inst_o,
-    pc_o
-  );
+module IF_ID (
+  input clk_i,
+  input [31:0] inst_i,
+  input [31:0] pc_i,
+  output reg [31:0] inst_o,
+  output reg [31:0] pc_o
+);
 
-    input clk_i;
-    input [31:0]  inst_i, pc_i;
-    output [31:0] inst_o, pc_o;
-    reg [31:0] inst_or = 32'd0, pc_or =32'd0;
-    assign inst_o = inst_or;
-    assign pc_o = pc_or;
+always @(posedge clk_i) begin
+  inst_o <= inst_i;
+  pc_o <= pc_i;
+end
 
-    always@(posedge clk_i) begin
-    inst_or <= inst_i;
-    pc_or <= pc_i;
-
-    end
-    endmodule
+endmodule
