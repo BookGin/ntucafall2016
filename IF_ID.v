9b6f1aeecc489e3404286dc900d259e06ed767c8
Bookgin <dongsheoil@gmail.com>
2016-12-08 00:33:29 +0800

    Fix bug: forget to initilize IF_ID reg to zero

--- IF_ID.v
+++ IF_ID.v
@@ -2,8 +2,8 @@ module IF_ID (
   input clk_i,
   input [31:0] inst_i,
   input [31:0] pc_i,
-  output reg [31:0] inst_o,
-  output reg [31:0] pc_o
+  output reg [31:0] inst_o = 32'd0,
+  output reg [31:0] pc_o = 32'd0
 );
 
 always @(posedge clk_i) begin
