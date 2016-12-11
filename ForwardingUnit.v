9a5238f113cdec7aee7e96d30b310fa44fb28d5f
littlehug <b03902081@csie.ntu.edu.tw>
2016-12-11 20:23:26 +0800

    revise output reg to wire

--- ForwardingUnit.v
+++ ForwardingUnit.v
@@ -2,35 +2,41 @@ module Forwarding
 (
   input EX_MEM_RegWrite,
   input MEM_WB_RegWrite,
-  input [4:0] EX_MEM_RegisterRd,
-  input [4:0] MEM_WB_RegisterRd,
-  input [4:0] ID_EX_RegisterRs,
-  input [4:0] ID_Ex_RegisterRt,
-  output reg [1:0] ForwardA,
-  output reg [1:0] ForwardB
+  input  [4:0] EX_MEM_RegisterRd,
+  input  [4:0] MEM_WB_RegisterRd,
+  input  [4:0] ID_EX_RegisterRs,
+  input  [4:0] ID_Ex_RegisterRt,
+  output [1:0] ForwardA,
+  output [1:0] ForwardB
 );
 
+reg [1:0] Forwarda;
+reg [1:0] Forwardb;
+
+assign ForwardA = Forwarda;
+assign ForwardB = Forwardb;
+
 always @(*) begin
-  ForwardA = 2'b00;
-  ForwardB = 2'b00;
+  Forwarda = 2'b00;
+  Forwardb = 2'b00;
   /*  EX Hazard */
   if (EX_MEM_RegWrite && 
       EX_MEM_RegisterRd != 5'b0 &&
       EX_MEM_RegisterRd == ID_EX_RegisterRs)
-      ForwardA = 2'b10;
+      Forwarda = 2'b10;
   if (EX_MEM_RegWrite &&
       EX_MEM_RegisterRd != 5'b0 &&
       EX_MEM_RegisterRd == ID_Ex_RegisterRt)
-      ForwardB = 2'b10;
+      Forwardb = 2'b10;
   /*  Mem Hazard */
   if (MEM_WB_RegWrite &&
       MEM_WB_RegisterRd != 5'b0 &&
       MEM_WB_RegisterRd == ID_EX_RegisterRs)
-      ForwardA = 2'b01;
+      Forwarda = 2'b01;
   if (MEM_WB_RegWrite &&
       MEM_WB_RegisterRd != 5'b0 &&
       MEM_WB_RegisterRd == ID_Ex_RegisterRt)
-      ForwardB = 2'b01;
+      Forwardb = 2'b01;
 end
 
 endmodule
