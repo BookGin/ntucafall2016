9f9e515f7e0916e2f33feb29f9b1e49b62770622
Jonah246 <b03902085@ntu.edu.tw>
2016-12-11 11:11:33 +0800

    ID_EX

--- ID_EX.v
+++ ID_EX.v
@@ -1,67 +1,38 @@
 module ID_EX
   (
-   clk_i,
-   inst_i,
-   pc_i,
-   RDData0_i,
-   RDData1_i,
-   SignExtended_i,
-   RDData0_o,
-   RDData1_o,
-   SignExtended_o,
-   inst_o,
-   pc_o,
-   //control
-   RegDst_o,
-   ALUOp_o,
-   ALUSrc_o,
-   RegWrite_o,
-   MemToReg_o,
-   MemWrite_o,
-   RegDst_i,
-   ALUOp_i,
-   ALUSrc_i,
-   RegWrite_i,
-   MemToReg_i,
-   MemWrite_i
+   input 	     clk_i,
+   input [31:0]      inst_i,
+   input [31:0]      pc_i,
+   input [31:0]      RDData0_i,
+   input [31:0]      RDData1_i,
+   input [31:0]      SignExtended_i,
+   output reg [31:0] RDData0_o = 32'd0,
+   output reg [31:0] RDData1_o = 32'd0,
+   output reg [31:0] SignExtended_o = 32'd0,
+   output reg [31:0] inst_o = 32'd0,
+   output reg [31:0] pc_o = 32'd0,
+		 //control
+   input 	     RegDst_i,
+   input [1:0] 	     ALUOp_i,
+   input 	     ALUSrc_i,
+   input 	     RegWrite_i,
+   input 	     MemToReg_i,
+   input 	     MemWrite_i,
+   output 	     RegDst_o,
+   output [1:0]      ALUOp_o,
+   output 	     ALUSrc_o,
+   output 	     RegWrite_o,
+   output 	     MemToReg_o,
+   output 	     MemWrite_o
    );
 
-   input clk_i;
-   input [31:0] inst_i, pc_i, RDData0_i, RDData1_i, SignExtended_i;
-   output [31:0] inst_o, pc_o, RDData0_o, RDData1_o, SignExtended_o;
-   reg [31:0] 	 inst_or= 32'd0, pc_or=32'd0, RDData0_or=32'd0, RDData1_or=32'd0, SignExtended_or=32'd0;
-
-   assign inst_o =inst_or;
-   assign pc_o = pc_or;
-   assign RDData0_o = RDData0_or;
-   assign RDData1_o = RDData1_or;
-   assign SignExtended_o = SignExtended_or;
-
-
    // Control
-   input 	 RegDst_i;
-   input [1:0] 	 ALUOp_i;
-   input 	 ALUSrc_i;
-   input 	 RegWrite_i;
-   input 	 MemToReg_i;
-   input 	 MemWrite_i;
-   input 	 IsBranch_i;
-   input 	 IsJump_i;
-
-   output 	 RegDst_o;
    reg 		 RegDst_or =0;
-   output [1:0]  ALUOp_o;
    reg [1:0] 	 ALUOp_or=2'd0;
-
-   output 	 ALUSrc_o;
    reg 		 ALUSrc_or=0;
-   output 	 RegWrite_o;
    reg 		 RegWrite_or=0;
-   output 	 MemToReg_o;
    reg 		 MemToReg_or=0;
-   output 	 MemWrite_o;
    reg 		 MemWrite_or=0;
-
 
    assign RegDst_o = RegDst_or;
    assign ALUOp_o = ALUOp_or;
@@ -71,12 +42,13 @@ module ID_EX
    assign MemWrite_o = MemWrite_or;
 
 
+
    always@(posedge clk_i) begin
-      RDData0_or <= RDData0_i;
-      RDData1_or <= RDData1_i;
-      SignExtended_or <= SignExtended_i;
-      pc_or <= pc_i;
-      inst_or <= inst_i;
+      RDData0_o <= RDData0_i;
+      RDData1_o <= RDData1_i;
+      SignExtended_o <= SignExtended_i;
+      pc_o <= pc_i;
+      inst_o <= inst_i;
       //control
       RegDst_or <= RegDst_i;
       ALUOp_or <= ALUOp_i;
