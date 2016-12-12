7af8ad3c5943b30d39048ebd755c8f1071ce661b
littlehug <b03902081@csie.ntu.edu.tw>
2016-12-12 13:20:56 +0800

    revise ALUOp input/output to be 2bits

--- MUX8.v
+++ MUX8.v
@@ -2,7 +2,7 @@ module MUX8 (
 	input IsHazzard_i,
 	
 	input RegDst_i, // EX
-	input ALUOp_i, // EX
+	input [1:0] ALUOp_i, // EX
 	input ALUSrc_i, // EX
 	input RegWrite_i, // WB
 	input MemToReg_i, // WB
@@ -10,7 +10,7 @@ module MUX8 (
 	input MemWrite_i, // M
 
 	output RegDst_o,
-	output ALUOp_o,
+	output [1:0] ALUOp_o,
 	output ALUSrc_o,
 	output RegWrite_o,
 	output MemToReg_o,
@@ -19,7 +19,7 @@ module MUX8 (
 );
 
 assign RegDst_o   = (IsHazzard_i)? 1'b0 : RegDst_i;
-assign ALUOp_o    = (IsHazzard_i)? 1'b0 : ALUOp_i;
+assign ALUOp_o    = (IsHazzard_i)? 2'b00 : ALUOp_i;
 assign ALUSrc_o   = (IsHazzard_i)? 1'b0 : ALUSrc_i;
 assign RegWrite_o = (IsHazzard_i)? 1'b0 : RegWrite_i;
 assign MemToReg_o = (IsHazzard_i)? 1'b0 : MemToReg_i;
