155c493cfd85edc04b531b970201d457b82bd3eb
littlehug <b03902081@csie.ntu.edu.tw>
2016-12-23 19:29:43 +0800

    revise the input file name in testbench_cache.v

--- testbench_cache.v
+++ testbench_cache.v
@@ -70,7 +70,7 @@ initial begin
 	end
 	
 	// Load instructions into instruction memory
-	$readmemb("instruction.txt", CPU.Instruction_Memory.memory);
+	$readmemb("instruction_cache.txt", CPU.Instruction_Memory.memory);
 	
 	// Open output file
 	outfile = $fopen("output.txt") | 1;
